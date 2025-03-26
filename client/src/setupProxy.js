989ad5f54a1c59169c5c4beebf35014ba8c762d1
wilfierd <hieulun76a@gmail.com>
2025-03-26 18:38:00 +0700

    for local test

---
+++ client/src/setupProxy.js
@@ -0,0 +1,14 @@
+// client/src/setupProxy.js
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:3000',
+      changeOrigin: true,
+      secure: false,
+      logLevel: 'debug'
+    })
+  );
+};
